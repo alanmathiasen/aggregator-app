735f098177d8de9769eaf854072bf2f79e8d5a61
Alan Mathiasen <alanemathiasen@gmail.com>
2024-01-21 18:48:53 -0300

    Add basic fetch functionality for publications

--- aggregator-client/src/main.tsx
+++ aggregator-client/src/main.tsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Publication from "./components/pages/Publication.tsx";
+import Home from "./components/pages/Home";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+// Create a client
+const queryClient = new QueryClient();
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/">
+      <Route index element={<Home />} />
+      <Route path="/publications/:id" element={<Publication />} />
+      {/* ... etc. */}
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
---
+++ aggregator-client/src/types/Publication.ts
@@ -0,0 +1,9 @@
+export interface Publication {
+  id: string;
+  title: string;
+  description: string;
+  rating: number;
+  image: string;
+  created_at: string;
+  updated_at: string;
+}
